498f4732a8e931a67650d68ea9b829aacb2f3b5d
hohin <e1121850@u.nus.edu>
2024-11-12 15:09:26 +0800

    nil

--- HoRS-ejb/src/java/ejb/session/ws/ReservationSystemWebService.java
+++ HoRS-ejb/src/java/ejb/session/ws/ReservationSystemWebService.java
@@ -7,13 +7,16 @@ package ejb.session.ws;
 import ejb.session.singleton.RoomAllocationSessionBeanLocal;
 import ejb.session.stateless.PartnerSessionBeanLocal;
 import ejb.session.stateless.ReservationSessionBeanLocal;
+import ejb.session.stateless.RoomRateSessionBeanLocal;
 import ejb.session.stateless.RoomSessionBeanLocal;
 import entity.Partner;
 import entity.Reservation;
 import entity.Room;
+import entity.RoomRate;
 import entity.RoomType;
 import exceptions.PartnerNotFoundException;
 import exceptions.RoomTypeNotFoundException;
+import java.math.BigDecimal;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
@@ -34,6 +37,9 @@ import javax.persistence.PersistenceContext;
 @Stateless()
 public class ReservationSystemWebService {
 
+    @EJB(name = "RoomRateSessionBeanLocal")
+    private RoomRateSessionBeanLocal roomRateSessionBeanLocal;
+
     @PersistenceContext(unitName = "HoRS-ejbPU")
     private EntityManager em;
     
@@ -45,6 +51,8 @@ public class ReservationSystemWebService {
     
     @EJB(name = "ReservationSessionBeanLocal")
     private ReservationSessionBeanLocal reservationSessionBeanLocal;
+    
+    
     
 
     @WebMethod(operationName = "partnerLogin")
@@ -74,7 +82,7 @@ public class ReservationSystemWebService {
     }
 
     @WebMethod
-    public Reservation partnerReserveRoom(@WebParam(name = "partnerID") Long partnerID,
+    public Long partnerReserveRoom(@WebParam(name = "partnerID") Long partnerID,
                                           @WebParam(name = "roomTypeId") Long roomTypeId,
                                           @WebParam(name = "checkInDate") Date checkInDate,
                                           @WebParam(name = "checkOutDate") Date checkOutDate,
@@ -83,6 +91,7 @@ public class ReservationSystemWebService {
             Date currentDate = new Date();
             boolean isImmediateCheckIn = checkInDate.equals(currentDate) && currentDate.toInstant().atZone(ZoneId.systemDefault()).getHour() >= 2;
             Long reservationID = reservationSessionBeanLocal.createReservationPartner(partnerID, roomTypeId, checkInDate, checkOutDate, isImmediateCheckIn);
+            return reservationID;
         } catch (Exception e) {
             System.out.println("Error during reservation: " + e.getMessage());
         }
@@ -120,4 +129,13 @@ public class ReservationSystemWebService {
             return null;
         }
     }
+    
+    @WebMethod
+    public BigDecimal calculateRateForRoomType(@WebParam(name = "roomType") RoomType roomType,
+                                               @WebParam(name = "checkInDate") Date checkInDate,
+                                               @WebParam(name = "checkOutDate") Date checkOutDate) {
+        BigDecimal rate = roomRateSessionBeanLocal.calculateRateForRoomType(roomType, checkInDate, checkOutDate);
+        return rate;
+    }
+    
 }
